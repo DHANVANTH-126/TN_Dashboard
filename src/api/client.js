c939bfe8efecd8c28e8f811ce07368f5ff8dceb1
Dhanvanth S <dhanvanthsureshkumar@gmail.com>
2026-03-17 15:05:37 +0530

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
-const API = process.env.VITE_API_URL;
+const API = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: `${API}/api`,
